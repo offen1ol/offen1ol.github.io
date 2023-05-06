4dc840290fc73bf2aa7eb7d9342eff62e16d90d6
Onna Offenbecher <onnaoffenbecher@Onnas-Laptop.local>
2023-05-06 00:14:41 -0400

    basic mobile menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+const menu = document.querySelector(".menu-icon");
+const links = document.querySelector("#mobile-menu");
 
+menu.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    links.classList.toggle("active");
+})
+
+document.querySelectorAll(".mobile-link").forEach(n => n.addEventListener("click", () => {
+    menu.classList.remove("active");
+    links.classList.remove("active");
+}))
